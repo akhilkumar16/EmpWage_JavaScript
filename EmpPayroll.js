bc0b28787e9be6915a5abfc2679b80bb4df780d8
Your Name <akhilkumarchandragiri@gmail.com>
2022-02-11 18:42:58 +0530

    [Akhil] uc13

--- EmpPayroll.js
+++ EmpPayroll.js
@@ -29,3 +29,47 @@ empPayroll.id=2;
 empPayroll.name = "Virat";
 empPayroll.salary = 50000;
 console.log(empPayroll.toString());
+//UC 13//
+    class EmployeePayroll
+    {
+        //properties of class
+        id;
+        salary;
+        gender;
+        startDate;
+        //defining constructor of class using constructor keeyword
+        constructor(...params)
+        {
+            this.id = params[0];
+            this.name = params[1];
+            this.salary = params[2];
+            this.gender = params[3];
+            this.startDate = params[4];
+        }
+        // getter and setter methods
+        get id() { return this._id; }
+        set id(id){ this._id = id; }
+        get name() { return this._name; }
+        set name(name) { this._name = name; }
+        get salary() { return this._salary; }
+        set salary(salary) { this._salary = salary; 
+        //methid to return everything in string
+        toString() 
+        {
+            const options = { year: 'numeric', month: 'long', day: 'numeric' };
+            const empDate = this.startDate === undefined ? "undefined" :
+                            this.startDate.toLocaleDateString("en-US", options);
+            return "Id = " + this.id + ", Name = " + this.name + ", Salary = " + this.salary + ", " +
+            "Gender = " + this.gender + ", StartDate = " + empDate;
+        }
+    }
+}
+    // Object for class using parameterized conbstructor
+    let empPayroll = new EmployeePayroll(1, "Mark", 30000);
+    console.log(empPayroll.toString());
+    // setting values to the properties of class
+    empPayroll.id=2;
+    empPayroll.name = "Virat";
+    console.log(empPayroll.toString());
+    let newEmployeePayroll = new EmployeePayroll(1, "rohit", 40000, "M", new Date());
+    console.log(newEmployeePayroll.toString());
